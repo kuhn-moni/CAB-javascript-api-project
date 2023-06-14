ea04c06856731970ff900515ebc8d1bd2c6fb07b
kuhn-moni <kuhn.moonay@gmail.com>
2023-06-14 12:59:13 +0200

    variables corrected

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
-// $(document).ready(function () {
-//   $("Index").DataTable();
-// });
+let data = myData; // this data is from response.js
+let totalTeams = data.length;
+console.log("totalTeams :>> ", totalTeams);
+console.log("data>>>>", data);
+let firstTeam = data[0]; //this is declaring a variable for the first object (ATL) in the array of teams
+console.log("firstTeam = data[0]", firstTeam);
+let secondTeam = data[1]; //this is an example to show the above point
 
-fetch("https://www.balldontlie.io/api/v1/players")
-  .then((res) => {
-    return res.json();
-  })
-  .then((data) => {
-    data.forEach((user) => {
-      const markup = <li>${user.name}</li>;
+// using the dom
+const myContainer = document.getElementById("team-container"); //"myContainer" is a variable created to link it to "team-container on HTML page" (variable referring to element in html via get ID)
+let firstElement = document.createElement("p"); // this is creating a "p" tag. you still need to add innertext to it.
+firstElement.innerText = firstTeam.name; //here we are adding linking firstTeam name property (firstTeam.nam). now the innertext exists in the p tag but u still need a to appendChild to make it visible.
+myContainer.appendChild(firstElement);
 
-      document.querySelector("ul").insertAdjacentHTML("beforeend", markup);
-    });
-  })
-  .catch((error) => console.log(error));
+// dynamically do the teams
+for (let i = 0; i < totalTeams; i++) {
+  console.log(i);
+  //   console.log(data[3]);
+}
