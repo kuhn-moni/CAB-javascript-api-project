d7c445f1e620b766fb0a4c4da40b4f5acfbc9182
kuhn-moni <kuhn.moonay@gmail.com>
2023-06-20 13:22:58 +0200

    player data partially added onto table. For loop tested but reverted back to for each

---
+++ allPlayers.js
@@ -0,0 +1,87 @@
+// *FETCHING THE DATA
+
+const getAllPlayers = () => {
+  const url = "https://www.balldontlie.io/api/v1/players";
+  fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then((result) => {
+      const playersData = result.data;
+      console.log("games :>> ", playersData);
+      controller(playersData);
+    })
+    .catch((error) => {
+      console.log("error :>> ", error);
+    });
+};
+
+// *PLAYER TABLE DATA
+const createPlayerTable = (playersData) => {
+  let table = document.getElementById("playerTable");
+  console.log("players data table", playersData);
+  //   table.innerText = "";
+
+  //   for (let i = 0; i < playersData.length; i++) {
+  //     let row = document.createElement("tr");
+  //     table.appendChild(row);
+
+  //     let column = document.createElement("td");
+  //     column.innerText = playersData[i].first_name;
+  //     row.appendChild(column);
+
+  //     let column2 = document.createElement("td");
+  //     column2.innerText = playersData[i].last_name;
+  //     row.appendChild(column2);
+
+  //     let column3 = document.createElement("td");
+
+  //     column3.innerText = playersData[i].position;
+  //     row.appendChild(column3);
+  //   }
+
+  playersData.forEach((player) => {
+    let row = document.createElement("tr");
+    table.appendChild(row);
+
+    let column = document.createElement("td");
+    column.innerText = player.first_name;
+    row.appendChild(column);
+
+    let column2 = document.createElement("td");
+    column2.innerText = player.last_name;
+    row.appendChild(column2);
+
+    let column3 = document.createElement("td");
+    column3.innerText = player.position;
+    row.appendChild(column3);
+  });
+};
+
+//* 3 GENERATE DROP DOWN
+const createDropDown = (playersData) => {
+  //   console.log("games in CreateDropdownFunction", playersData);
+  const dropdown = document.getElementById("leagueDropdown");
+
+  const competitionsArray = playersData.map((playersData) => {
+    return playersData.last_name;
+  });
+};
+
+//* 4 MAKE CONTROLLER FUNCTION
+
+function controller(playersData) {
+  console.log("controllers", playersData);
+  //get the data
+  //we call fetchData outside of this function
+  // build table with all data
+  createPlayerTable(playersData);
+
+  //generate DropDown filter options
+  //   createDropDown(playersData);
+  //create filter functions
+  //   setEventListeners(playersData);
+  // set event listeners
+}
+
+getAllPlayers();
--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 //* 1 fetching the data...
 
-const getAllPlayers = () => {
+const getAllTeams = () => {
   const url = "https://www.balldontlie.io/api/v1/players";
   fetch(url)
     .then((response) => {
@@ -17,7 +17,7 @@ const getAllPlayers = () => {
     });
 };
 
-getAllPlayers();
+getAllTeams();
 // *RAUL example - once function is declared, you must run the function at the bottom
 // const doSometing = () => {
 //   console.log("doing something");
@@ -26,6 +26,29 @@ getAllPlayers();
 // document.getElementById("button").addEventListener("click", doSometing);
 
 //* 2 function for creating table and dropdown
+
+// *TEAM TABLE DATA
+const createTeamTable = () => {
+  let table = document.getElementById("teamTable");
+
+  teams.forEach((team, i) => {
+    let row = document.createElement("tr");
+    table.appendChild(row);
+
+    let column = document.createElement("td");
+    column.innerText = team.name;
+    row.appendChild(column);
+
+    let column2 = document.createElement("td");
+    column2.innerText = team.city;
+    row.appendChild(column2);
+
+    let column3 = document.createElement("td");
+
+    column3.innerText = team.conference;
+    row.appendChild(column3);
+  });
+};
 
 //* 3 generate Dropdown options
 
